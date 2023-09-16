386635a0f1a1d0bea1b5aaac3df050fa819e76fd
Tekkieware <tekkieware@gmail.com>
2023-09-16 18:18:58 +0100

    fav-toggle-fix

--- src/components/Favorite.tsx
+++ src/components/Favorite.tsx
@@ -5,12 +5,13 @@ import {BsFillSuitHeartFill} from 'react-icons/bs'
 const Favorite:React.FC<{id: number}> = ({id}) => {
 
   const [isFavorite, setIsFavorite] = useState<boolean>(false)
-  useEffect(()=>{
-    console.log(isFavorite)
-}, [isFavorite])
+
+function fave(id:number){
+    setIsFavorite(!isFavorite)
+}
 
   return (
-    <div className='fav-icon text-center py-1' onClick={() => setIsFavorite(!isFavorite)}><BsFillSuitHeartFill style={{ color: `${isFavorite ? "#BE123C": "#D1D5D" }`, fontSize: "1.1em" }} /></div>
+    <div className='fav-icon text-center py-1' onClick={() => fave(id)}><BsFillSuitHeartFill style={{ color: `${isFavorite ? "#BE123C": "#D1D5DB" }`, fontSize: "1.1em" }} /></div>
   )
 }
 
